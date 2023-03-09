8894e5835cd3ce60424b69c409782a0a283782c7
gwendal le bris <gwen.coding@gmail.com>
2023-03-09 23:05:24 +0100

    Define user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "rooms#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :rooms, only: [:show] do
+    resources :bookings, only: [:create]
+  end
+
+  get "/localisation", to: "pages#localisation"
+  get "/contact", to: "pages#contact"
 end
