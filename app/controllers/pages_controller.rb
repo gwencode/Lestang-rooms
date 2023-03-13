19e5d503adad58c7e4bd81e493cb6154285d2033
gwendal le bris <gwen.coding@gmail.com>
2023-03-13 10:08:10 +0100

    Add mapbox and infos on localisation page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[localisation contact]
 
   def localisation
+    @markers = [ { lat: 43.667737, lng: 1.501558 } ]
   end
 
   def contact
