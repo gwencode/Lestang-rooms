e3d7c6551de8f7f1d22b6d0830500c21ec3b4b11
gwendal le bris <gwen.coding@gmail.com>
2023-03-16 19:25:21 +0100

    Begin to style homepage

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,14 +5,19 @@ class RoomsController < ApplicationController
   def index
     @maison = Room.first
     @chambre = Room.last
+    image_files = Dir.glob(Rails.root.join('app', 'assets', 'images', "Home", '*.{jpg,jpeg,png,gif}'))
+    image_paths = image_files.map { |image_path| "Home/#{File.basename(image_path)}" }
+    @two_first_image_paths = image_paths.first(2)
+    last_image_paths = image_paths.drop(2)
+    @last_two_image_paths = last_image_paths.each_slice(2).to_a
   end
 
   def show
-    image_files = Dir.glob(Rails.root.join('app', 'assets', 'images', "#{@room.name}", '*.{jpg,jpeg,png,gif,PNG}'))
+    image_files = Dir.glob(Rails.root.join('app', 'assets', 'images', "#{@room.name}", '*.{jpg,jpeg,png,gif}'))
     @image_paths = image_files.map { |image_path| "#{@room.name}/#{File.basename(image_path)}" }
     @last_image_paths = @image_paths.drop(1)
 
-    sleep_files = Dir.glob(Rails.root.join('app', 'assets', 'images', "#{@room.name}", 'sleep', '*.{jpg,jpeg,png,gif,PNG}'))
+    sleep_files = Dir.glob(Rails.root.join('app', 'assets', 'images', "#{@room.name}", 'sleep', '*.{jpg,jpeg,png,gif}'))
     @sleep_image_paths = sleep_files.map { |image_path| "#{@room.name}/sleep/#{File.basename(image_path)}" }
   end
 
