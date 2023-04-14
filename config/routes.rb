315df8e9c4069ffa12854519abd2286b96105200
gwendal le bris <gwen.coding@gmail.com>
2023-04-14 11:41:16 +0200

    just a change to commit and push

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   root to: "rooms#index"
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+
   resources :rooms, only: [:show] do
     resources :bookings, only: [:create]
   end
 
   get "/localisation", to: "pages#localisation"
   get "/contact", to: "pages#contact"
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
