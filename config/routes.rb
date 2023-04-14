be07e063a28fffcc1cc286a9a697672c8dc61f97
gwendal le bris <gwen.coding@gmail.com>
2023-04-14 11:41:47 +0200

    just a change to commit and push

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,6 @@ Rails.application.routes.draw do
   get "/contact", to: "pages#contact"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
