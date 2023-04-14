dd6abbc85e261f8a4a084925d345a40b68b1bd49
gwendal le bris <gwen.coding@gmail.com>
2023-04-14 08:35:36 +0200

    Small text fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
 
   def localisation
     @markers = [
-      { lat: 43.671038,
-        lng: 1.497738,
+      { lat: 43.668226,
+        lng: 1.498332,
         info_window_html: render_to_string(partial: "info_window"),
         marker_html: render_to_string(partial: "marker")
         }
