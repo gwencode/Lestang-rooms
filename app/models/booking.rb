e8f13c96687ec95070f41a56e4014faf51349416
gwendal le bris <gwen.coding@gmail.com>
2023-04-14 10:50:17 +0200

    Seeds: real bookigns for Mai. Booking: real calculation prices

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -7,11 +7,55 @@ class Booking < ApplicationRecord
   # validate :start_date_after_today
   # validate :room_available
 
-  def duration
-    (self.end_date - self.start_date) / 60 / 60 / 24
+  def nights
+    ((end_date - start_date) / 60 / 60 / 24).round
+  end
+
+  def night_price
+    case guests_number
+    when 7
+      room.price_per_day + 10
+    when 8
+      room.price_per_day + 20
+    else
+      room.price_per_day
+    end
+  end
+
+  def basic_price
+    nights * night_price
+  end
+
+  def night_price_week_reduction
+    case room.name
+    when "La Maison"
+      night_price - 32
+    when "La Chambre"
+      night_price - 6
+    end
+  end
+
+  def week_reduction
+    if nights >= 7
+      case room.name
+      when "La Maison"
+        - nights * 32
+      when "La Chambre"
+        - nights * 6
+      end
+    else
+      0
+    end
+  end
+
+  def cleaning_fee
+    room.name == "La Maison" ? 60 : 0
   end
 
   def calculate_booking_price
-    self.duration.round * room.price_per_day
+    price = basic_price
+    price += week_reduction
+    price += cleaning_fee
+    return price
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,15 +9,19 @@
 puts "Cleaning database..."
 Review.destroy_all
 Booking.destroy_all
+
+### Comment 2 next lines after first time in production
 Room.destroy_all
 User.destroy_all
 
 puts "Database cleaned!"
 
+### Comment next lines after first time in production
+
 puts "Creating 2 admin users..."
 
 User.create(
-  email: "erle@me.com",
+  email: "erle22@hotmail.fr",
   password: "password",
   first_name: "Erle",
   last_name: "Le Bris",
@@ -25,7 +29,7 @@ User.create(
 )
 
 User.create(
-  email: "emilie@me.com",
+  email: "emilie.aubry59@gmail.com",
   password: "password",
   first_name: "Emilie",
   last_name: "Aubry",
@@ -36,7 +40,7 @@ puts "2 admin users created!"
 
 puts "Creating 2 normal users..."
 
-coco = User.create(
+User.create(
   email: "coco@me.com",
   password: "password",
   first_name: "Corentin",
@@ -44,7 +48,7 @@ coco = User.create(
   admin: false
 )
 
-gwen = User.create(
+User.create(
   email: "gwen@me.com",
   password: "password",
   first_name: "Gwendal",
@@ -58,11 +62,11 @@ puts "Creating 2 rooms..."
 
 description_maison = "En séjournant dans notre maison, vous pourrez profiter d'un cadre de vie chaleureux et convivial, avec une décoration soignée qui reflète notre personnalité et notre style de vie. Notre maison est équipée de tout ce dont vous aurez besoin pour passer un séjour confortable, notamment une cuisine entièrement équipée, une salle de bains moderne et des chambres confortables."
 
-maison = Room.create(
+Room.create(
   name: "La Maison",
   description: description_maison,
   max_guests: 8,
-  price_per_day: 75,
+  price_per_day: 160,
   arrival_hour: "à partir de 14:00",
   departure_hour: "avant 12:00",
   bedrooms: 3,
@@ -72,58 +76,96 @@ maison = Room.create(
 
 description_chambre = "Nous proposons une chambre confortable, lumineuse et climatisée, équipée d'un lit Queen size et d'une commode pour ranger vos effets personnels. Vous aurez également accès au reste des parties communes partagées avec nous."
 
-chambre = Room.create(
+Room.create(
   name: "La Chambre",
   description: description_chambre,
   max_guests: 2,
-  price_per_day: 25,
+  price_per_day: 40,
   arrival_hour: "entre 18:00 et 22:00",
   departure_hour: "avant 11:00",
   bedrooms: 1,
   beds: 1,
-  bathrooms: 1,
+  bathrooms: 1
 )
 
 puts "2 rooms created!"
 
-puts "Creating 3 bookings..."
+### Finsih commenting lines after first time in production
+
+coco = User.find_by(first_name: "Corentin")
+gwen = User.find_by(first_name: "Gwendal")
+maison = Room.find_by(name: "La Maison")
+chambre = Room.find_by(name: "La Chambre")
+
+puts "Creating 6 bookings..."
 
 booking1 = Booking.new(
   user: coco,
   room: maison,
-  start_date: DateTime.new(2023, 5, 5, 14, 0, 0),
-  end_date: DateTime.new(2023, 5, 7, 11, 0, 0),
+  start_date: DateTime.new(2023, 5, 1, 14, 0, 0),
+  end_date: DateTime.new(2023, 5, 13, 11, 0, 0),
   guests_number: 4,
-  status: "pending"
+  status: "approved"
 )
 booking1.booking_price = booking1.calculate_booking_price
 booking1.save
 
 booking2 = Booking.new(
-  user: coco,
-  room: chambre,
-  start_date: DateTime.new(2023, 5, 8, 14, 0, 0),
-  end_date: DateTime.new(2023, 5, 9, 11, 0, 0),
-  guests_number: 2,
-  status: "pending"
+  user: gwen,
+  room: maison,
+  start_date: DateTime.new(2023, 5, 21, 14, 0, 0),
+  end_date: DateTime.new(2023, 6, 9, 11, 0, 0),
+  guests_number: 8,
+  status: "approved"
 )
 booking2.booking_price = booking2.calculate_booking_price
 booking2.save
 
 booking3 = Booking.new(
-  user: gwen,
+  user: coco,
   room: chambre,
-  start_date: DateTime.new(2023, 5, 10, 14, 0, 0),
-  end_date: DateTime.new(2023, 5, 12, 11, 0, 0),
-  guests_number: 1,
+  start_date: DateTime.new(2023, 4, 25, 14, 0, 0),
+  end_date: DateTime.new(2023, 5, 1, 11, 0, 0),
+  guests_number: 2,
   status: "approved"
 )
 booking3.booking_price = booking3.calculate_booking_price
 booking3.save
 
-puts "3 bookings created!"
+booking4 = Booking.new(
+  user: gwen,
+  room: chambre,
+  start_date: DateTime.new(2023, 5, 5, 14, 0, 0),
+  end_date: DateTime.new(2023, 5, 9, 11, 0, 0),
+  guests_number: 1,
+  status: "approved"
+)
+booking4.booking_price = booking4.calculate_booking_price
+booking4.save
 
-Booking.all.each { |booking| p booking }
+booking5 = Booking.new(
+  user: coco,
+  room: chambre,
+  start_date: DateTime.new(2023, 5, 12, 14, 0, 0),
+  end_date: DateTime.new(2023, 5, 22, 11, 0, 0),
+  guests_number: 2,
+  status: "pending"
+)
+booking5.booking_price = booking5.calculate_booking_price
+booking5.save
+
+booking6 = Booking.new(
+  user: gwen,
+  room: chambre,
+  start_date: DateTime.new(2023, 5, 26, 14, 0, 0),
+  end_date: DateTime.new(2023, 5, 29, 11, 0, 0),
+  guests_number: 1,
+  status: "pending"
+)
+booking6.booking_price = booking6.calculate_booking_price
+booking6.save
+
+puts "6 bookings created!"
 
 puts "Creating reviews..."
 
@@ -149,6 +191,5 @@ Ce fût un très bon séjour pour nous qui étions 7 avec tout le nécessaire à
 Review.create(author: author3, content: content3, room_id: maison.id)
 
 puts "Reviews created!"
-
 
 puts "Finished!"
