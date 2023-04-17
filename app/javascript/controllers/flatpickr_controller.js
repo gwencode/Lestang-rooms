dac2e9bcae1ff2d1b10c8ad3b596daaa8267e441
gwendal le bris <gwen.coding@gmail.com>
2023-04-17 10:37:45 +0200

    Begin to custom flatpickr

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,12 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
+// Import the rangePlugin from the flatpickr library
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
 // Connects to data-controller="flatpickr"
 export default class extends Controller {
   static targets = [ "startTime", "endTime" ]
 
   connect() {
-    flatpickr(this.startTimeTarget, {})
-    flatpickr(this.endTimeTarget, {})
+    flatpickr(this.startTimeTarget, {
+      altInput: true,
+      altFormat: "d/m/Y",
+      // Provide an id for the plugin to work
+      plugins: [new rangePlugin({ input: "#end_date"})]
+
+      // enableTime: true
+    })
+    flatpickr(this.endTimeTarget, {
+      altInput: true,
+      altFormat: "d/m/Y",
+
+      // enableTime: true
+    })
   }
 }
