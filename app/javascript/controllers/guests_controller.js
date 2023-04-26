2d248c94a39dd705f75d01116e3b8bf65a14d036
gwendal le bris <gwen.coding@gmail.com>
2023-04-26 19:45:08 +0200

    Begin JS calculation of prices on room show

--- app/javascript/controllers/guests_controller.js
+++ app/javascript/controllers/guests_controller.js
@@ -2,21 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="guests"
 export default class extends Controller {
-  static targets = ["price"]
+  static targets = ["guests", "prices", "arrival", "departure"]
   static values = { maxGuests: Number }
 
   connect() {
+    console.log(this.pricesTarget)
   }
 
   decrease() {
-    if (1 < parseInt(this.priceTarget.value)) {
-      this.priceTarget.value = parseInt(this.priceTarget.value) - 1
+    if (1 < parseInt(this.guestsTarget.value)) {
+      this.guestsTarget.value = parseInt(this.guestsTarget.value) - 1
     }
   }
 
   increase() {
-    if (parseInt(this.priceTarget.value) < this.maxGuestsValue) {
-      this.priceTarget.value = parseInt(this.priceTarget.value) + 1
+    if (parseInt(this.guestsTarget.value) < this.maxGuestsValue) {
+      this.guestsTarget.value = parseInt(this.guestsTarget.value) + 1
+    }
+    console.log(this.arrivalTarget.value)
+    console.log(typeof(this.arrivalTarget.value))
+  }
+
+  setDates() {
+    let dateStr = this.arrivalTarget.value
+    if (dateStr.length > 11) {
+      let arrival = dateStr.substring(0, 10);
+      let departure = dateStr.substring(14);
+      let date2 = new Date(departure);
+      let date1 = new Date(arrival);
+      let diff = date2.getTime() - date1.getTime();
+      let days = Math.ceil(diff / (1000 * 3600 * 24));
+      // console.log(days)
     }
   }
 }
