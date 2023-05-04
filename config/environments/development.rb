fbf273dcdcefd9337119689aba08042bb919a085
gwendal le bris <gwen.coding@gmail.com>
2023-05-04 14:06:46 +0200

    Configure Mailer in development environnement

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,11 @@ Rails.application.configure do
   # Localtunnel
   config.hosts << /\A[a-z\d-]+\.loca\.lt\z/
 
+  # Action Mailer
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
