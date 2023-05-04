ea27a609587371e2b449ccb75af5240e7b5e8b99
gwendal le bris <gwen.coding@gmail.com>
2023-05-04 14:09:34 +0200

    Add confirmable and lockable to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable
 
   has_many :bookings
   has_many :rooms, through: :bookings
