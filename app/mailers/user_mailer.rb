7292343c32213796ff231da01ad84c4a32ab9cb0
gwendal le bris <gwen.coding@gmail.com>
2023-05-10 10:13:19 +0200

    Change url of host for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
-  default from: 'gwen.coding@gmail.com'
+  default from: ENV['SENDMAIL_USERNAME']
 
-  def welcome_email
-    @user = params[:user]
-    @url = 'http://localhost:3000/users/sign_in'
-    mail(to: @user.email, subject: 'Bienvenue sur Résidence Lestang !')
-  end
+  # def welcome_email
+  #   @user = params[:user]
+  #   @url = 'http://localhost:3000/users/sign_in'
+  #   mail(to: @user.email, subject: 'Bienvenue sur Résidence Lestang !')
+  # end
 
   def message_email
     @user = params[:user]
