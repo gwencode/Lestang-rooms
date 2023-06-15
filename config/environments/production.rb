c6ed9cf7d3e0cf30c6e9e1f82b3241753ff7293d
gwendal le bris <gwen.coding@gmail.com>
2023-06-15 14:07:00 +0200

    Change host production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  host = "residence-lestang.herokuapp.com"
+  host = "residence-lestang.com"
 
   config.action_mailer.default_url_options = { host: host }
   # Settings specified here will take precedence over those in config/application.rb.
