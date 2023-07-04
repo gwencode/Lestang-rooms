568adfb085f9e6d601cd6e90c77df6db98fa827b
gwendal le bris <gwen.coding@gmail.com>
2023-07-04 15:15:07 +0200

    Friendly-id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,7 +23,7 @@ class PagesController < ApplicationController
       user.first_name = user_params[:first_name]
       user.last_name = user_params[:last_name]
     end
-    room = Room.find(params[:room].to_i)
+    room = Room.friendly.find(params[:room].to_i)
     message = params[:message]
     if user.email.present? && user.first_name.present? && user.last_name.present? && message.present?
       MessageMailer.with(user: user, room: room, message: message).contact_admin_email.deliver_now
