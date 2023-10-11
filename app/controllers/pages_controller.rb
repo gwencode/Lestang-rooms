122fdfa173d0e3f14acb8d3537f7dafc7c11dfa2
gwendal le bris <gwen.coding@gmail.com>
2023-10-11 10:31:07 +0200

    Feature: Add a Recaptcha on Contact Page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,7 +29,8 @@ class PagesController < ApplicationController
     end
     room = Room.friendly.find(params[:room].to_i)
     message = params[:message]
-    if user.email.present? && user.first_name.present? && user.last_name.present? && message.present?
+    check = verify_recaptcha action: 'message', minimum_score: 0.7, secret_key: ENV['RECAPTCHA_SECRET_KEY']
+    if check && user.email.present? && user.first_name.present? && user.last_name.present? && message.present?
       MessageMailer.with(user: user, room: room, message: message).contact_admin_email.deliver_now
       MessageMailer.with(user: user, room: room, message: message).contact_user_email.deliver_now
       redirect_to root_path, notice: "Votre message a bien été envoyé."
