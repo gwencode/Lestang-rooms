cdc793d5fd6f698dc21bb207ae9a811fef4b8013
Gwendal Le Bris <gwen.coding@gmail.com>
2023-10-25 17:54:59 +0200

    Remove captcha from sign-up and sign-in

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  prepend_before_action :validate_recaptchas, only: [:create]
+  # prepend_before_action :validate_recaptchas, only: [:create]
 
   # GET /resource/sign_up
   # def new
@@ -13,8 +13,51 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   # def create
-  #   super
-  #   # UserMailer.with(user: @user).welcome_email.deliver_now
+  #   user_params = params[:user]
+  #   first_name = user_params[:first_name]
+  #   last_name = user_params[:last_name]
+  #   email = user_params[:email]
+  #   password = user_params[:password]
+  #   password_confirmation = user_params[:password_confirmation]
+
+  #   @user = User.new(
+  #     first_name: first_name,
+  #     last_name: last_name,
+  #     email: email,
+  #     password: password,
+  #     password_confirmation: password_confirmation
+  #   )
+
+  #   if @user.valid?
+  #     @valid_captcha = validate_recaptchas
+  #     if @valid_captcha
+  #       super
+  #     else
+  #       respond_with_navigational(@user) { render :new }
+  #     end
+  #   else
+  #     super
+  #   end
+  # end
+
+  # def create
+  #   @user = User.new user_params
+  #   @user.validate # this line will validate the user even if Recaptcha failed. This way we will present all potential validation errors right away
+
+  #   check = verify_recaptcha action: 'signup', minimum_score: 0.1, secret_key: ENV['RECAPTCHA_SECRET_V3'] ||
+  #   (verify_recaptcha model: @user, secret_key: ENV['RECAPTCHA_SECRET_V2'])
+
+  #   if check && @user.save
+  #      # everything is great, you can now let the user in and redirect them somewhere
+  #      super
+  #   else
+
+  #     @user.validate # add any other validation errors
+
+  #     # @user.validate generates a new errors, so that recaptcha error message cannot be seen.
+  #     @user.errors.add(:base, t('recaptcha.errors.verification_failed')) unless check
+  #     render :new # if something goes wrong, we'll re-render the form
+  #   end
   # end
 
   # GET /resource/edit
@@ -43,17 +86,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
-  def validate_recaptchas
-    v3_verify = verify_recaptcha(action: 'signup',
-                                 minimum_score: 0.9,
-                                 secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
-    v2_verify = verify_recaptcha(secret_key: ENV['RECAPTCHA_SECRET_KEY_V2'])
+  # def validate_recaptchas
+  #   # @v3_verify = verify_recaptcha(action: 'signup',
+  #   #                              minimum_score: 1,
+  #   #                              secret_key: ENV['RECAPTCHA_SECRET_KEY_V3'])
+  #   v2_verify = verify_recaptcha(secret_key: ENV['RECAPTCHA_SECRET_KEY_V2'])
 
-    return if v3_verify || v2_verify
+  #   return if v2_verify
 
-    self.resource = resource_class.new sign_up_params
-    respond_with_navigational(resource) { render :new }
-  end
+  #   self.resource = resource_class.new sign_up_params
+  #   respond_with_navigational(resource) { render :new }
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
